0cf36a4c0e0b7eb8287651c8b5d9484ddec086f0
Woodenspoon <wmwarzecha@gmail.com>
2024-12-28 17:09:23 +0100

    feat: add numeric and deletion buttons interaction

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ function divide(a, b){
 let leftValue = '0';
 let rightValue = '0';
 let operator = '';
+let buttonArray = {};
 
 const BUTTON_VALUES = [
-    'AC','DEL','_','+',
+    'AC','DEL','+/-','+',
     '7','8','9','-',
     '4','5','6','*',
     '1','2','3','+',
@@ -44,10 +45,51 @@ function operate(leftValue, operator, rightValue){
 }
 
 function createButton(buttonType){
-    const button = document.createElement('div');
+    const button = document.createElement('button');
+    button.type = "button";
     button.classList.add("btn");
     button.textContent = buttonType;
     return button;
+}
+
+function updateDisplay(){
+    display.textContent = rightValue;
+}
+
+function updateValue(c){
+    if(rightValue.at(0) === '0'){
+        rightValue = c;
+    }
+    else{
+        rightValue += c;
+    }
+}
+
+function deleteFromValue(){
+    if(rightValue.length === 2 && rightValue.at(0) === '-'){
+            rightValue ='0';
+    }
+    else if(rightValue.length > 1){
+        rightValue = rightValue.slice(0, -1);
+    }
+    else{
+        rightValue = '0';
+    }
+}
+
+function resetAll(){
+    leftValue = '0';
+    rightValue = '0';
+    operator = '';
+}
+
+function changeSign(){
+    if(rightValue.at(0) === '-'){
+        rightValue = rightValue.slice(1);
+    }
+    else if(rightValue.at(0) !== '0'){
+        rightValue = '-' + rightValue;
+    }
 }
 
 const body = document.querySelector("body");
@@ -58,22 +100,42 @@ calculator.classList.add("calculator");
 const display = document.createElement("div");
 display.classList.add("display");
 
-display.textContent = "Some crazy 1 + 1"
-
-
 const buttons = document.createElement("div");
 buttons.classList.add("buttons");
 
-// for(let i = 0; i < 20; i++){
-//     buttons.appendChild(createButton());
-// };
-
 BUTTON_VALUES.forEach(value => {
-    buttons.appendChild(createButton(value));
+    newButton = createButton(value);
+    buttons.appendChild(newButton);
+    buttonArray[value] = newButton;
 });
 
+for(let i = 0; i <= 9; i++){
+    console.log(buttonArray[i])
+    buttonArray[i].addEventListener("click", () => {
+        updateValue(`${i}`);
+        updateDisplay();
+    });
+}
+
+buttonArray.DEL.addEventListener("click", () => {
+    deleteFromValue();
+    updateDisplay();
+});
+
+buttonArray.AC.addEventListener("click", () => {
+    resetAll();
+    updateDisplay();
+});
+
+buttonArray['+/-'].addEventListener("click", () => {
+    changeSign();
+    updateDisplay();
+});
 
 calculator.appendChild(display);
 calculator.appendChild(buttons);
 
 body.appendChild(calculator);
+
+//initial display state
+updateDisplay();
